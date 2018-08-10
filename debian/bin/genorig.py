fac6a14424bf09327c882227d29355a7159e8ceb
Ben Hutchings <ben@decadent.org.uk>
2018-08-10 14:23:14 +0100

    Remove our private patch system, which is no longer needed
    I converted the main patch series to quilt format in 3.2.20-1, but
    this patch system was still used by genorig.py.  One useful
    feature that was not available elsewhere was using patch + unifdef
    to exclude only part of a source file.  However no source files
    have needed this since 3.16-rc4 and I don't expect this to ever
    be needed again.
    
    The preceding changes moved the file removal/exclusion list into
    debian/copyright and the disabling of broken features into the
    main patch series, so the private patch system isn't doing
    anything.
    
    So we can now remove it completely.

--- debian/bin/genorig.py
+++ debian/bin/genorig.py
@@ -14,7 +14,6 @@ import time
 import warnings
 
 from debian_linux.debian import Changelog, VersionLinux
-from debian_linux.patches import PatchSeries
 
 
 class Main(object):
@@ -52,7 +51,7 @@ class Main(object):
             if len(self.input_files) > 1:
                 self.upstream_patch(self.input_files[1])
 
-            # debian_patch() will change file mtimes.  Capture the
+            # exclude_files() will change dir mtimes.  Capture the
             # original release time so we can apply it to the final
             # tarball.  Note this doesn't work in case we apply an
             # upstream patch, as that doesn't carry a release time.
@@ -62,7 +61,6 @@ class Main(object):
                     os.stat(os.path.join(self.dir, self.orig, 'Makefile'))
                     .st_mtime))
 
-            self.debian_patch()
             self.exclude_files()
             os.umask(old_umask)
             self.tar(orig_date)
@@ -125,13 +123,6 @@ class Main(object):
         cmdline.append('| (cd %s; patch -p1 -f -s -t --no-backup-if-mismatch)' % os.path.join(self.dir, self.orig))
         if os.spawnv(os.P_WAIT, '/bin/sh', ['sh', '-c', ' '.join(cmdline)]):
             raise RuntimeError("Can't patch source")
-
-    def debian_patch(self):
-        name = "orig"
-        self.log("Patching source with debian patch (series %s)\n" % name)
-        fp = open("debian/patches/series-" + name)
-        series = PatchSeries(name, "debian/patches", fp)
-        series(dir=os.path.join(self.dir, self.orig))
 
     def exclude_files(self):
         self.log("Excluding file patterns specified in debian/copyright\n")
